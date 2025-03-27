0ea4fd580ee6cf444765abfadbbdbdac5f46d593
Nick De Breuck <nick.debreuck@outlook.com>
2025-03-27 22:21:18 +0000

    [development/allocators] removed commented debug allocator test code from test global allocator and moved it into its own file

--- tests/1_engine/rex_engine_unit_test/src/memory/global_allocators/test_global_debug_allocator.cpp
+++ tests/1_engine/rex_engine_unit_test/src/memory/global_allocators/test_global_debug_allocator.cpp
@@ -1,16 +1,20 @@
 #include "rex_unit_test/rex_catch2.h"
 
-#include "rex_engine/memory/global_allocators/global_allocator.h"
+#include "rex_engine/memory/global_allocators/global_debug_allocator.h"
 
 #include "rex_unit_test/test_object.h"
 
-TEST_CASE("TEST - Global Allocator - Normal Allocation")
+TEST_CASE("TEST - Global Debug Allocator - Normal Allocation")
 {
-	rex::GlobalAllocator alloc;
+	rex::GlobalDebugAllocator alloc;
 
 	s32 size = 10;
 	void* ptr1 = alloc.allocate(size);
+#if REX_DISABLE_DEBUG_ALLOCATION
+	REX_CHECK(ptr1 == nullptr);
+#else
 	REX_CHECK(ptr1 != nullptr);
+#endif
 	alloc.deallocate(ptr1, size);
 
 	using namespace rex::test;
@@ -27,6 +31,16 @@ TEST_CASE("TEST - Global Allocator - Normal Allocation")
 
 	test_object* ptr2 = alloc.allocate<test_object>();
 
+#if REX_DISABLE_DEBUG_ALLOCATION
+	REX_CHECK(ptr2 == nullptr);
+	REX_CHECK(test_object::num_created() == 0);
+	REX_CHECK(test_object::num_ctor_calls() == 0);
+	REX_CHECK(test_object::num_copy_ctor_calls() == 0);
+	REX_CHECK(test_object::num_move_ctor_calls() == 0);
+	REX_CHECK(test_object::num_dtor_calls() == 0);
+	REX_CHECK(test_object::num_copy_assignment_calls() == 0);
+	REX_CHECK(test_object::num_move_assignment_calls() == 0);
+#else
 	REX_CHECK(ptr2 != nullptr);
 	REX_CHECK(test_object::num_created() == 1);
 	REX_CHECK(test_object::num_ctor_calls() == 1);
@@ -35,9 +49,19 @@ TEST_CASE("TEST - Global Allocator - Normal Allocation")
 	REX_CHECK(test_object::num_dtor_calls() == 0);
 	REX_CHECK(test_object::num_copy_assignment_calls() == 0);
 	REX_CHECK(test_object::num_move_assignment_calls() == 0);
+#endif
 
 	alloc.deallocate(ptr2);
 
+#if REX_DISABLE_DEBUG_ALLOCATION
+	REX_CHECK(test_object::num_created() == 0);
+	REX_CHECK(test_object::num_ctor_calls() == 0);
+	REX_CHECK(test_object::num_copy_ctor_calls() == 0);
+	REX_CHECK(test_object::num_move_ctor_calls() == 0);
+	REX_CHECK(test_object::num_dtor_calls() == 0);
+	REX_CHECK(test_object::num_copy_assignment_calls() == 0);
+	REX_CHECK(test_object::num_move_assignment_calls() == 0);
+#else
 	REX_CHECK(test_object::num_created() == 1);
 	REX_CHECK(test_object::num_ctor_calls() == 1);
 	REX_CHECK(test_object::num_copy_ctor_calls() == 0);
@@ -45,4 +69,5 @@ TEST_CASE("TEST - Global Allocator - Normal Allocation")
 	REX_CHECK(test_object::num_dtor_calls() == 1);
 	REX_CHECK(test_object::num_copy_assignment_calls() == 0);
 	REX_CHECK(test_object::num_move_assignment_calls() == 0);
+#endif
 }
