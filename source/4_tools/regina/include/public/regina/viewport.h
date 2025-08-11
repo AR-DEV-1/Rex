9cd7e1166bf1fb9a40c28ae346f4ce07e155c241
Nick De Breuck <nick.debreuck@outlook.com>
2025-08-11 23:01:42 +0100

    [development/viewport_rendering] first code of the viewport written

--- source/4_tools/regina/include/public/regina/viewport.h
+++ source/4_tools/regina/include/public/regina/viewport.h
@@ -1,16 +1,28 @@
 #pragma once
 
 #include "rex_engine/engine/types.h"
+#include "rex_engine/assets/tilemap.h"
 #include "rex_std/string.h"
+#include "rex_std/bonus/math.h"
 
 namespace regina
 {
 	class Viewport
 	{
 	public:
-		Viewport(rsl::string_view name);
+		Viewport(rsl::string_view name, rex::Tilemap* tilemap);
+
+		void update();
+
+		rsl::pointi32 top_left_from_camera_pos(rsl::pointi32 cameraPos);
 
 	private:
 		rsl::string m_name;
+		rex::Tilemap* m_tilemap;
+		rsl::pointi32 m_camera_pos;
+		f32 m_min_pixels_per_tile;
+		f32 m_max_pixels_per_tile;
+		f32 m_zoom_step_rate;
+		f32 m_current_num_pixels_per_tile;
 	};
 }
--- source/4_tools/regina/src/viewport.cpp
+++ source/4_tools/regina/src/viewport.cpp
@@ -2,7 +2,37 @@
 
 namespace regina
 {
-	Viewport::Viewport(rsl::string_view name)
+	Viewport::Viewport(rsl::string_view name, rex::Tilemap* tilemap)
 		: m_name(name)
+		, m_tilemap(tilemap)
+		, m_min_pixels_per_tile(1.0f) // Can't zoom further out than 1 pixel per tile
+		, m_max_pixels_per_tile(8.0f) // Can't zoom in beyond 8 pixels per tile as this is the zoom of the gameboy
+		, m_zoom_step_rate(0.125f)
+		, m_current_num_pixels_per_tile(m_max_pixels_per_tile)
 	{}
+
+	void Viewport::update()
+	{
+		// based on the camera position, create a tilemap, the same size as the viewport's resolution
+		// the camera has a zoom. more tiles are drawn depending on zoom
+
+		rsl::pointi32 top_left = top_left_from_camera_pos(m_camera_pos);
+	}
+
+	rsl::pointi32 Viewport::top_left_from_camera_pos(rsl::pointi32 cameraPos)
+	{
+		s32 widget_width = 0;
+		s32 widget_height = 0;
+
+		f32 num_pixels_per_tile = static_cast<s32>(m_current_num_pixels_per_tile);
+		f32 widget_width_in_tiles = widget_width * num_pixels_per_tile;
+		f32 widget_height_in_tiles = widget_height * num_pixels_per_tile;
+
+		rsl::pointi32 top_left = cameraPos;
+		top_left.x -= widget_width_in_tiles * 0.5f;
+		top_left.y -= widget_height_in_tiles * 0.5f;
+
+		return top_left;
+	}
+
 }
